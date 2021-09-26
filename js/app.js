ba5e80c4fe2f35ea32b6ede1ebdc50cb76574ac5
israelisaque <israelisaque@hotmail.com>
2021-09-26 20:45:59 -0300

    Created Quiz

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('.quiz-form');
+const finalScoreContainer = document.querySelector('.final-score-container');
+
+const correctAnswers = ['D', 'B', 'C', 'A'];
+
+let score = 0;
+
+const getUserAnswers = () => correctAnswers.map((_, index) => form[`inputQuestion${index + 1}`].value);
+
+const calculateUserScore = userAnswers => {
+  userAnswers.forEach((userAnswer, index) => {
+    const isUserAnswerCorrect = userAnswer === correctAnswers[index];
+
+    if (isUserAnswerCorrect) {
+      score += 25
+    }
+  });
+}
+
+const showFinalScore = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+  finalScoreContainer.classList.remove('d-none');
+}
+
+const animateFinalScore = () => {
+  let counter = 0;
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer);
+    }
+
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`
+  }, 10);
+}
+
+const resetUserScore = () => {
+  score = 0;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const userAnswers = getUserAnswers();
+
+  resetUserScore();
+  calculateUserScore(userAnswers);
+  showFinalScore();
+  animateFinalScore();
+});
